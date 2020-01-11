bc657e74757fd45ea46627a9a5e051ea5e7b22eb
Julian Tuberquia <jtuberquia07@outlook.com>
2020-01-11 11:29:27 -0500

    mvn test compile

--- vars/ciMaven.groovy
+++ vars/ciMaven.groovy
@@ -49,7 +49,7 @@ def call() {
             stage('Build Docker Image') {
                 steps {
                     script {
-                        def dockerfile = libraryResource 'Dockerfile'
+                        def dockerfile = libraryResource 'resoures/Dockerfile'
                         writeFile file: 'Dockerfile', text: dockerfile
                         sh "ls -l"
                     }
